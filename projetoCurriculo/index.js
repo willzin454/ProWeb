a308b1a9b36a7c70e364670e07e27403d4e009ac
willzin454 <williamcarvalho493@gmail.com>
2024-07-08 22:27:55 -0300

    Aulas

--- projetoCurriculo/index.js
+++ projetoCurriculo/index.js
@@ -1,19 +1,29 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     verificarTema();
-    localizacao();
-    verificarLocalizcao();
+    verificarLocalizacaoStorage();
+    apresentarCurriculo();
+    lerCurriculo();
 });
 
-function localizacao(){
+function verificarLocalizacaoStorage(){
+    const localizacao = localStorage.getItem("localizacao");
+    if(localizacao){
+        document.getElementById("localizacao").value = localizacao;
+    }else{
+        getLocalizacao();
+    }
+}
+
+function getLocalizacao(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition
-        (position=>{
+        navigator.geolocation.getCurrentPosition(position=>{
             const lat = position.coords.latitude;
             const long = position.coords.longitude;
             fetch(`https://geocode.xyz/${lat},${long}?geoit=json`).then(response=>response.json())
             .then(data=>{
                 const localizacao = data.region || data.city || `${lat}, ${long}`;
                 document.getElementById("localizacao").value=localizacao;
+                localStorage.setItem("localizacao", localizacao);
             }).catch(error=>console.error("Algo de errado não esta certo!", error));
         });
     }
@@ -33,8 +43,54 @@ function alterarTema(){
     localStorage.setItem("tema", novoTema);
 };
 
-function verificarLocalizcao(){
-    const local = document.body.getAttribute("local");
-    document.body.setAttribute("local", local);
-    localStorage.setItem("local", local);
+function gerarCurriculo(){
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const localizacao = document.getElementById("localizacao").value;
+    const experiencia = document.getElementById("experiencia").value;
+    const habilidade = document.getElementById("habilidades").value;
+
+    const curriculo = {nome, email, telefone, localizacao, experiencia, habilidade};
+    console.log(curriculo);
+    localStorage.setItem("curriculo", JSON.stringify(curriculo));
+    apresentarCurriculo(curriculo);
+    vibrar();
+}
+
+function apresentarCurriculo(data){
+    const template = document.getElementById("templateCurriculo").content.cloneNode(true);
+    template.querySelector(".nome").textContent = data.nome;
+    template.querySelector(".email").textContent = data.email; 
+    template.querySelector(".telefone").textContent = data.telefone;
+    template.querySelector(".localizacao").textContent = data.localizacao;
+    template.querySelector(".experiencia").textContent = data.experiencia;
+    template.querySelector(".habilidades").textContent = data.habilidade;
+
+    const mostrarCurriculo = document.getElementById("mostrarCurriculo");
+    mostrarCurriculo.innerHTML="";
+    mostrarCurriculo.appendChild(template);
+}
+
+function vibrar(){
+    if(navigator.vibrate){
+        navigator.vibrate(10);
+    }
+}
+
+function lerCurriculo(){
+    const curriculo = JSON.parse(localStorage.getItem("curriculo"))
+    if(curriculo){
+        apresentarCurriculo(curriculo);
+    }
+}
+
+function copiar(){
+    const curriculo = document.getElementById("mostrarCurriculo").textContent;
+    navigator.clipboard.writeText(curriculo).then(()=>{
+        alert("Conteudo copiado para area de transferencia")
+    }).catch(error=>{
+        console.error("Erro ao copiar", error);
+        alert("Erro ao copiar o conteudo");
+    })
 }
